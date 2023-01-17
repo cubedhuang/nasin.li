5bb207090d517e4f9e5eea5f809048088adf94d2
cubedhuang <99danielh@gmail.com>
2023-01-16 21:18:06 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+</svelte:head>
+
+<nav class="px-8 bg-slate-800 shadow-lg">
+	<ul
+		class="max-w-5xl mx-auto py-4 flex items-center justify-between gap-4 font-bold text-white text-lg"
+	>
+		<li>
+			<a href="/">nasin.li</a>
+		</li>
+		<li>
+			<a href="/login">login</a>
+		</li>
+	</ul>
+</nav>
+
+<div class="px-8 py-8">
+	<div class="max-w-5xl mx-auto">
+		<slot />
+	</div>
+</div>
